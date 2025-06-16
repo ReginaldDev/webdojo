f18e592ce7e9f3d31fa20653b29f899ee893e640
Reginaldo Silva de Albuquerque Junior <reginaldo.devtech@gmail.com>
2025-06-16 11:15:13 -0300

    new lessons

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -5,5 +5,30 @@ describe('Formulário de Consultoria', ()=>{
         cy.submitLogin('papito@webdojo.com', 'katana123')
 
         cy.goTo('Formulários', 'Consultoria')
+
+        cy.get('input[placeholder="Digite seu nome completo"]').type('Fernando Papito')
+
+        cy.get('input[placeholder="Digite seu email"]').type('teste@webdojo.com')
+            
+        cy.get('#phone').type('18999999999').should('have.value', '(18) 99999-9999')   
+       
+        cy.contains('label', 'Tipo de Consultoria')
+            .parent()
+            .find('select')
+            .select('Individual')
+       
+        cy.contains('label', 'Pessoa Física')
+            .find('input')
+            .check()
+            .should('be.checked')  
+            
+        cy.contains('label', 'Pessoa Jurídica')
+            .find('input')
+            .should('be.not.checked')   
+        
+        cy.contains('label', 'CPF')
+            .parent().find('input')
+            .type('310.207.860-51') 
+            .should('have.value', '310.207.860-51')   
     })
 })
